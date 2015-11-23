5e57a4e5d09380543d593fffcb6597b6862a3f02
raditotev <raditotev@gmail.com>
2015-11-23 12:05:49 +0000

    Populated the DB with airports and flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Airports
+
+Airport.create([{name: "ZAG"},
+                       {name: "PRG"},
+                       {name: "ORY"},
+                       {name: "FRA"},
+                       {name: "STR"}])
+
+# Flights
+
+date = Time.now + 1.month
+Flight.create(departing_airport: Airport.find(2),
+                    arrival_airport: Airport.first,
+                    date: date,
+                    duration: "2 hours and 30 mins")
+
+Flight.create(departing_airport: Airport.first,
+                    arrival_airport: Airport.find(3),
+                    date: date,
+                    duration: "2 hours")
+
+Flight.create(departing_airport: Airport.first,
+                    arrival_airport: Airport.find(4),
+                    date: date,
+                    duration: "1 hour 30 mins")
+
+Flight.create(departing_airport: Airport.first,
+                    arrival_airport: Airport.find(5),
+                    date: date,
+                    duration: "less than 1 hour")
+
+Flight.create(departing_airport: Airport.first,
+                    arrival_airport: Airport.find(6),
+                    date: date,
+                    duration: "1 hour")
+
+Flight.create(departing_airport: Airport.first,
+                    arrival_airport: Airport.find(7),
+                    date: date,
+                    duration: "1 hour")
